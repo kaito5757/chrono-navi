570bfd13f75f5ed7fa66c03b2cf37b540d131de2
konno <kaito.121855@gmail.com>
2025-01-01 17:36:13 +0900

    add: Docker導入

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "standalone",
+  webpack: (config) => {
+    config.watchOptions = {
+      poll: 300,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
+};
 
 export default nextConfig;
