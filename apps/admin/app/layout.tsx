6da8babd661f0665715bbab9569490f03df70f48
konno <kaito.121855@gmail.com>
2025-01-04 13:29:19 +0900

    shadcsの導入

--- apps/admin/app/layout.tsx
+++ apps/admin/app/layout.tsx
@@ -1,3 +1,4 @@
+import "@repo/ui/globals.css";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
--- apps/admin/app/page.tsx
+++ apps/admin/app/page.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@repo/ui/button";
+import { Button } from "@repo/ui/components/button";
 import type { ImageProps } from "next/image";
 import Image from "next/image";
 import styles from "./page.module.css";
@@ -64,9 +64,7 @@ export default function Home() {
             Read our docs
           </a>
         </div>
-        <Button appName="admin" className={styles.secondary}>
-          Open alert
-        </Button>
+        <Button>Open alert</Button>
       </main>
       <footer className={styles.footer}>
         <a
