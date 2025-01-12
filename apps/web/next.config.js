d694e57553ce2ec0affe8686725f10aefda3c251
konno <kaito.121855@gmail.com>
2025-01-12 17:04:32 +0900

    update: docker修正

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,3 @@
-import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@repo/shadcn-ui"],
@@ -11,9 +9,5 @@ const nextConfig = {
     return config;
   },
 };
-
-if (process.env.NODE_ENV === "development") {
-  await setupDevPlatform();
-}
 
 export default nextConfig;
