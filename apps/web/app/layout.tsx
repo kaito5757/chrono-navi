451473a807cb5180456bbf00d4b2e0a0aceac86c
konno <kaito.121855@gmail.com>
2025-01-28 11:36:56 +0900

    nextのサービス分け（Docker）

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,8 +2,6 @@ import "@repo/tailwind-config/globals.css";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 
-export const dynamic = "force-dynamic";
-
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
